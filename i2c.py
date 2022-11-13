27bf5ced7ec247573b626468cb56a420c4f4a2ca
salmansafari <118105451+salmansafari@users.noreply.github.com>
2022-11-13 17:50:55 +0530

    Add files via upload

---
+++ i2c.py
@@ -0,0 +1,10 @@
+'''
+GND -> GND (pin 6)
+VCC -> 5V (pin 4)
+SDA -> SDA (pin 3)
+SCL -> SCL (pin 5)
+'''
+# Enable the I2C Interface
+# wget https://rb.gy/f9bcwc to download the code            OR
+# wget https://bitbucket.org/MattHawkinsUK/rpispy-misc/raw/master/python/lcd_i2c.py
+# pip3 install smbus smbus2
